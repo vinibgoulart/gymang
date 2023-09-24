e0dc1fea81a72fc852878c5e7c3e70673e6500e5
Vinicius <viblaziusgoulart@gmail.com>
2023-09-24 00:39:17 -0300

    feat(page): page header component

---
+++ apps/web/src/components/PageHeader.tsx
@@ -0,0 +1,27 @@
+import { Flex, Heading } from '@chakra-ui/react';
+
+type PageHeaderProps = {
+  title: string;
+  subtitle?: string;
+  actions?: React.ReactNode;
+};
+
+export const PageHeader = (props: PageHeaderProps) => {
+  const getHeading = () => {
+    if (props.subtitle) {
+      return (
+        <Heading size={'lg'}>
+          {props.title}: {props.subtitle}
+        </Heading>
+      );
+    }
+
+    return <Heading size={'lg'}>{props.title}</Heading>;
+  };
+  return (
+    <Flex justifyContent={'space-between'}>
+      {getHeading()}
+      {props.actions}
+    </Flex>
+  );
+};
