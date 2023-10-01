1adab2175f5eb5148ccee1f54783cfca0001fb9c
Vinicius <viblaziusgoulart@gmail.com>
2023-10-01 18:40:45 -0300

    feat(workout-split): not allow to create a workout split to another user

--- apps/server/src/modules/workoutSplit/mutations/__tests__/WorkoutSplitAddMutation.spec.ts
+++ apps/server/src/modules/workoutSplit/mutations/__tests__/WorkoutSplitAddMutation.spec.ts
@@ -8,6 +8,7 @@ import {
 } from '@gymang/testutils';
 import { handleCreateUser } from '@gymang/user';
 import { handleCreateWorkout } from '@gymang/workout';
+import { handleCreateWorkoutSplit } from '@gymang/workout-split';
 import { graphql } from 'graphql';
 import { toGlobalId } from 'graphql-relay';
 
@@ -41,7 +42,7 @@ const query = `
   }
 `;
 
-it.only('should add a new workout split', async () => {
+it('should add a new workout split', async () => {
   const user = await handleCreateUser();
   const workout = await handleCreateWorkout({ user });
 
@@ -86,4 +87,87 @@ it.only('should add a new workout split', async () => {
   );
 
   expect(sanitizeTestObject(result.data)).toMatchSnapshot();
+});
+
+it('should not add a new workout split if already existes 7 workout split', async () => {
+  const user = await handleCreateUser();
+  const workout = await handleCreateWorkout({ user });
+
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+  await handleCreateWorkoutSplit({ user, workout });
+
+  const input = {
+    name: 'Chest Workout',
+    workout: toGlobalId('Workout', workout._id),
+    modality: WORKOUT_SPLIT_MODALITY.BODYBUILDING,
+  };
+
+  const variables = {
+    input,
+  };
+
+  const context = await getContext({
+    graphql: GRAPHQL_TYPE.WEB,
+    user,
+  });
+
+  const rootValue = {};
+
+  const result = await graphql({
+    schema: schemaWeb,
+    source: query,
+    rootValue,
+    contextValue: context,
+    variableValues: variables,
+  });
+
+  expect(result.errors).toBeUndefined();
+
+  expect(result.data.WorkoutSplitAdd.error).toEqual(
+    'You can not create more than 7 Workout Splits',
+  );
+  expect(result.data.WorkoutSplitAdd.success).toBeNull();
+});
+
+it('should not add a new workout split for another user', async () => {
+  const user = await handleCreateUser();
+  const anotherUser = await handleCreateUser();
+  const workout = await handleCreateWorkout({ user: anotherUser });
+
+  const input = {
+    name: 'Chest Workout',
+    workout: toGlobalId('Workout', workout._id),
+    modality: WORKOUT_SPLIT_MODALITY.BODYBUILDING,
+  };
+
+  const variables = {
+    input,
+  };
+
+  const context = await getContext({
+    graphql: GRAPHQL_TYPE.WEB,
+    user,
+  });
+
+  const rootValue = {};
+
+  const result = await graphql({
+    schema: schemaWeb,
+    source: query,
+    rootValue,
+    contextValue: context,
+    variableValues: variables,
+  });
+
+  expect(result.errors).toBeUndefined();
+
+  expect(result.data.WorkoutSplitAdd.error).toEqual(
+    'You can not create a Workout Split for another user',
+  );
+  expect(result.data.WorkoutSplitAdd.success).toBeNull();
 });
